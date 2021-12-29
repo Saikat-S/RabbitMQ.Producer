c5859b7f696988e54dd9cc2e2d23e206b6c905d3
Saikat Sharma <saikatsharma4@gmail.com>
2021-12-29 18:19:08 +0600

    init project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            FanoutExchangePublisher.Publish(channel); 
+        }
+
+    }
+}
